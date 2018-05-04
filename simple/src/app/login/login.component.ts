3b0dca7bd746381c737c8b1541396f6f7344ee0f
kakaylectio <kakay.lectio@gmail.com>
2018-05-04 04:36:03 -0700

    Added login page front-end with no backend.

--- simple/src/app/login/login.component.ts
+++ simple/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	email: string;
+	password: string;
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
   }
